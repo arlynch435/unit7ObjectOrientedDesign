4a1b0737cc5ac6781c72007bd1604e4eba327257
arlynch435 <arlynch@NN123-HA-20.HS.sd203.org>
2016-02-01 15:08:32 -0600

    Started card heirarchy

---
+++ chapter9practice/CallingCard.java
@@ -0,0 +1,11 @@
+public class CallingCard extends Card
+{
+    /** description of instance variable x (add comment for each instance variable) */
+    private String idNumber;
+
+    public CallingCard(String n, String id)
+   {  
+      super(n);
+      idNumber = id;
+   }
+}
